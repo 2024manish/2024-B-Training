2e74d3067a7eabc3b0cf5e84d88aa5be8de6cf70
2024manish <121475109+2024manish@users.noreply.github.com>
2023-07-19 13:36:39 +0530

    Update reverse of a string.py

--- 2000290120090/week2/stack/reverse of a string.py
+++ 2000290120090/week2/stack/reverse of a string.py
@@ -1,35 +1,32 @@
 # Python program to reverse a string using stack
 
-# Function to create an empty stack.
-# It initializes size of stack as 0
+
 
 
 def createStack():
 	stack = []
 	return stack
 
-# Function to determine the size of the stack
+
 
 
 def size(stack):
 	return len(stack)
 
-# Stack is empty if the size is 0
+
 
 
 def isEmpty(stack):
 	if size(stack) == 0:
 		return true
 
-# Function to add an item to stack .
-# It increases size by 1
+
 
 
 def push(stack, item):
 	stack.append(item)
 
-# Function to remove an item from stack.
-# It decreases size by 1
+
 
 
 def pop(stack):
@@ -60,4 +57,4 @@ string = "oraganic farming"
 string = reverse(string)
 print("Reversed string is " + string)
 
-# This code is contributed by Sunny Karira
+
